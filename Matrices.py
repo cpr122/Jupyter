18e41c3b915769b17ad54b407d623ebedebb25b3
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-12-01 19:51:03 -0600

    Update Matrices.py

--- Matrices.py
+++ Matrices.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+# Crear una matriz 3x3
+matriz = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Acceder a un elemento
+print(matriz[1, 2])  # Imprime 6
+
+# Sumar dos matrices
+otra_matriz = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+resultado = matriz + otra_matriz
+print(resultado)
+ 
